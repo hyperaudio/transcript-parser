1e0c1f4782bb3bb9e53d246eb4392b2255b79c3f
Laurian Gridinoc <laurian@gridinoc.name>
2017-10-08 01:26:37 +0300

    paragraphs, words filter, multiple text nodes support

--- src/index.js
+++ src/index.js
@@ -3,43 +3,68 @@ import htmlparser from 'htmlparser2';
 const parse = html => new Promise((resolve, reject) => {
   const json = {
     transcript: ``,
-    words: [{}],
+    words: [],
     paragraphs: [],
   };
 
-  const stack = [];
+  let currentWord = {};
+  let currentParagraph;
 
   const parser = new htmlparser.Parser({
     onopentag: (name, attrs) => {
       const word = {
-        tag: name
+        // tag: name
       };
+
+      if (name === `p`) {
+        currentParagraph = {};
+        json.paragraphs.push(currentParagraph);
+      }
 
       if (attrs[`data-m`]) {
         word.start = parseInt(attrs[`data-m`]) / 1e3;
         if (attrs[`data-d`]) word.end = word.start + parseInt(attrs[`data-d`]) / 1e3;
       }
 
+      if (attrs[`data-t`]) {
+        const [start, duration] = attrs[`data-t`].split(`,`);
+        word.start = word.end = parseFloat(start);
+        if (!isNaN(parseFloat(duration))) word.end = word.start + parseFloat(duration);
+      }
+
+      if (currentParagraph) {
+        if (!currentParagraph.start && word.start) {
+          currentParagraph.start = currentParagraph.end = word.start;
+        }
+        if (word.end) currentParagraph.end = word.end;
+      }
+
       json.words.push(word);
-      stack.push({name, attrs});
+      currentWord = word;
     },
 
     ontext: text => {
-      const word = json.words[json.words.length - 1];
+      const word = currentWord;
 
-      if (word.text) {
-        word.text += text;
-      } else word.text = text;
+      word.text = text.trim();
 
-      if (!word.startOffset) word.startOffset = json.transcript.length;
+      word.startOffset = json.transcript.length;
       json.transcript += text;
       word.endOffset = json.transcript.length;
+
+      if (json.words.length === 0 || word !== json.words[json.words.length - 1]) json.words.push(word);
     },
 
-    // onclosetag: name => {
-    // },
+    onclosetag: name => {
+      if (name === `p`) currentParagraph = null;
+      currentWord = {};
+    },
 
-    onend: () => resolve(json),
+    onend: () => {
+      json.words = json.words.filter(word => !!word.text);
+      resolve(json);
+    },
+
     onerror: error => reject(error)
   }, {
     decodeEntities: true,
