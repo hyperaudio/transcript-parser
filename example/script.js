a198c21273538fe2ddfcefc6bfadfc59f9a1471e
Laurian Gridinoc <laurian@gridinoc.name>
2017-10-08 00:19:07 +0300

    html parsing and simple conversion

--- example/script.js
+++ example/script.js
@@ -1,1 +1,6 @@
 const haJson = require(`../`);
+const html = require(`./H6ZcHSkET3eXppEpWirw6g.json`).content;
+
+haJson(html).then(json => {
+  console.log(JSON.stringify(json, null, 2));
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-export default () => {
-  console.log(`hello module`);
+import htmlparser from 'htmlparser2';
+
+const parse = html => new Promise((resolve, reject) => {
+  const json = {
+    transcript: ``,
+    words: [{}],
+    paragraphs: [],
+  };
+
+  const stack = [];
+
+  const parser = new htmlparser.Parser({
+    onopentag: (name, attrs) => {
+      const word = {
+        tag: name
+      };
+
+      if (attrs[`data-m`]) {
+        word.start = parseInt(attrs[`data-m`]) / 1e3;
+        if (attrs[`data-d`]) word.end = word.start + parseInt(attrs[`data-d`]) / 1e3;
+      }
+
+      json.words.push(word);
+      stack.push({name, attrs});
+    },
+
+    ontext: text => {
+      const word = json.words[json.words.length - 1];
+
+      if (word.text) {
+        word.text += text;
+      } else word.text = text;
+
+      if (!word.startOffset) word.startOffset = json.transcript.length;
+      json.transcript += text;
+      word.endOffset = json.transcript.length;
+    },
+
+    // onclosetag: name => {
+    // },
+
+    onend: () => resolve(json),
+    onerror: error => reject(error)
+  }, {
+    decodeEntities: true,
+    lowerCaseTags: true,
+    lowerCaseAttributeNames: true,
+    recognizeSelfClosing: true,
+  });
+
+  parser.write(html);
+  parser.end();
+});
+
+export default html => {
+  return parse(html);
 };
