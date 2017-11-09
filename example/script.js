72f8c76e7c291749560a0916290193bd28f75952
Laurian Gridinoc <laurian@gridinoc.name>
2017-11-09 13:10:58 +0100

    extra example

--- example/script.js
+++ example/script.js
@@ -1,5 +1,7 @@
 const haJson = require(`../`);
-const html = require(`./H6ZcHSkET3eXppEpWirw6g.json`).content;
+
+// const html = require(`./H6ZcHSkET3eXppEpWirw6g.json`).content;
+const html = require(`./rojWGnK_Q0-xd9WRuPP04A`).content;
 
 haJson(html).then(json => {
   console.log(JSON.stringify(json, null, 2));
